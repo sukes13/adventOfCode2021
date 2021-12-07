e9e2ec505be95b384e06f1f2905445a086653fc1
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-07 22:00:57 +0100

    Day 2: removed internal from test funs

--- src/test/java/be/sukes/adventofcode/day2/Day2MoveTest.kt
+++ src/test/java/be/sukes/adventofcode/day2/Day2MoveTest.kt
@@ -6,25 +6,25 @@ import org.junit.jupiter.api.Test
 class Day2MoveTest{
 
     @Test
-    internal fun `start position - do move 'forward 2' - position is 2, depth is 0`() {
+    fun `start position - do move 'forward 2' - position is 2, depth is 0`() {
         val actual : Submarine = Submarine().doMove("forward 2")
 
         assertThat(actual).isEqualTo(Submarine(2,0))
     }
 
     @Test
-    internal fun `start position - do move 'up 2' - position is 0, depth is 2`() {
+    fun `start position - do move 'up 2' - position is 0, depth is 2`() {
         val actual : Submarine = Submarine().doMove("up 2")
 
         assertThat(actual).isEqualTo(Submarine(0,2))
     }
 
-    @Test
-    fun `test move - getHorizontal - is 15`() {
+//    @Test
+//    fun `test move - getHorizontal - is 15`() {
 //        val moves : List<String> = FileReader().readLines("/day2/testMoves.txt")
 //
 //        val actual : Int = Day2Move().doMoves(moves)
 //
 //        assertThat(actual).isEqualTo(15)
-    }
+//    }
 }
