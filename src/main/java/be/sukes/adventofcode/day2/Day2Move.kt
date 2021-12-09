56a39618ee222a07961f37ff48d9704f7b3a54e8
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-09 21:21:49 +0100

    Day 2: solved part 1 + first refactor of multiple

--- src/main/java/be/sukes/adventofcode/day2/Day2Move.kt
+++ src/main/java/be/sukes/adventofcode/day2/Day2Move.kt
@@ -2,21 +2,52 @@ package be.sukes.adventofcode.day2
 
 class Day2Move {
     fun doMoves(moves: List<String>): Submarine {
-        return Submarine().doMove(moves.first())
+        var submarine = Submarine(aim = 0)
+        for (move in moves)
+            submarine = submarine.doMove(move)
+
+        return submarine
     }
 }
 
-data class Submarine(val position : Int = 0, val depth : Int = 0){
+data class Submarine(val position: Int = 0,
+                     val depth: Int = 0,
+                     val aim: Int = 0){
+
     fun doMove(move: String): Submarine {
-        val commandAsString = move.trim().split(" ")
-        when(commandAsString[0]){
-            "forward" -> return this.copy(position = position.plus(commandAsString[1].toInt()))
-            "up" -> return this.copy(depth = depth.plus(commandAsString[1].toInt()))
+        val moveCommand : MoveCommand = parseMove(move)
+        when(moveCommand){
+            is MoveForward -> return this.copy(position = position.plus(moveCommand.value))
+            is AimUp -> return this.copy(depth = depth.minus(moveCommand.value))
+            is AimDown -> return this.copy(depth = depth.plus(moveCommand.value))
+        }
+
+        throw IllegalMoveException()
+    }
+
+    private fun parseMove(move: String) : MoveCommand {
+        val commandAsStrings = move.trim().split(" ")
+        val (command,value) = Pair(commandAsStrings[0],commandAsStrings[1].toInt())
+        when (command) {
+            "forward" -> return MoveForward(value)
+            "up" -> return AimUp(value)
+            "down" -> return AimDown(value)
         }
         throw IllegalMoveException()
     }
 
+    open class MoveCommand(val value : Int)
+    class MoveForward(value: Int) : MoveCommand(value)
+    class AimUp(value: Int) : MoveCommand(value)
+    class AimDown(value: Int) : MoveCommand(value)
+
+
+    fun solution(): Int {
+        return position * depth
+    }
+
 }
+
 
 class IllegalMoveException : Throwable()
 
--- src/test/java/be/sukes/adventofcode/day2/Day2MoveTest.kt
+++ src/test/java/be/sukes/adventofcode/day2/Day2MoveTest.kt
@@ -1,30 +1,64 @@
 package be.sukes.adventofcode.day2
 
+import be.sukes.adventofcode.import.FileReader
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day2MoveTest{
 
     @Test
-    fun `start position - do move 'forward 2' - position is 2, depth is 0`() {
+    fun `start position - do move 'forward 2' - position = 2, depth = 0, aim = 0`() {
         val actual : Submarine = Submarine().doMove("forward 2")
 
-        assertThat(actual).isEqualTo(Submarine(2,0))
+        assertThat(actual).isEqualTo(Submarine(2,0,0))
     }
 
     @Test
-    fun `start position - do move 'up 2' - position is 0, depth is 2`() {
+    fun `start position - do move 'up 2' - position is 0, depth is -2`() {
         val actual : Submarine = Submarine().doMove("up 2")
 
-        assertThat(actual).isEqualTo(Submarine(0,2))
+        assertThat(actual).isEqualTo(Submarine(0, -2, 0))
     }
 
-//    @Test
-//    fun `test move - getHorizontal - is 15`() {
-//        val moves : List<String> = FileReader().readLines("/day2/testMoves.txt")
-//
-//        val actual : Int = Day2Move().doMoves(moves)
-//
-//        assertThat(actual).isEqualTo(15)
-//    }
+    @Test
+    fun `start position - do move 'down 2' - position is 0, depth is 2`() {
+        val actual : Submarine = Submarine().doMove("down 2")
+
+        assertThat(actual).isEqualTo(Submarine(0, 2, 0))
+    }
+
+    @Test
+    fun `start position - do moves 'down 2', 'up 5' - position is 0, depth is -3`() {
+        val actual : Submarine = Submarine().doMove("down 2")
+                                            .doMove("up 5")
+
+        assertThat(actual).isEqualTo(Submarine(0, -3, 0))
+    }
+
+    @Test
+    fun `test moves - position is 15, depth is 10`() {
+        val moves : List<String> = FileReader().readLines("/day2/testMoves.txt")
+
+        val actual : Submarine = Day2Move().doMoves(moves)
+
+        assertThat(actual).isEqualTo(Submarine(15, 10, 0))
+    }
+
+    @Test
+    fun `moves - position is 1944, depth is 1049`() {
+        val moves : List<String> = FileReader().readLines("/day2/moves.txt")
+
+        val actual : Submarine = Day2Move().doMoves(moves)
+
+        assertThat(actual).isEqualTo(Submarine(1944, 1049, 0))
+    }
+
+    @Test
+    fun `moves - solution = 2039256`() {
+        val moves : List<String> = FileReader().readLines("/day2/moves.txt")
+
+        val actual : Int = Day2Move().doMoves(moves).solution()
+
+        assertThat(actual).isEqualTo(2039256)
+    }
 }
