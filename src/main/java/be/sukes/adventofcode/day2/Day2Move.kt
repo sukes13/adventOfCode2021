895ad493ce8d6ea553bc1ff50da3e705b4218816
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-10 18:27:35 +0100

    Day 2: refactor: moved submarine-update functions back into Submarine

--- src/main/java/be/sukes/adventofcode/day2/Day2Move.kt
+++ src/main/java/be/sukes/adventofcode/day2/Day2Move.kt
@@ -14,36 +14,45 @@ data class Submarine(val position: Int = 0,
                      val depth: Int = 0,
                      val aim: Int = 0){
 
-    fun doMove(move: String): Submarine = parseMove(move).execute(this)
+    fun doMove(move: String): Submarine = move.asCommand().execute(this)
 
-    private fun parseMove(move: String) : MoveCommand {
-        val commandAsStrings = move.trim().split(" ")
-        val (command,value) = commandAsStrings[0] to commandAsStrings[1].toInt()
-        when (command) {
-            "forward" -> return MoveForward(value)
-            "up" -> return AimUp(value)
-            "down" -> return AimDown(value)
-        }
-        throw IllegalMoveException()
-    }
-
-    interface MoveCommand{
-        fun execute(submarine: Submarine) : Submarine
-    }
-    class MoveForward(val value: Int) : MoveCommand {
-        override fun execute(submarine: Submarine) : Submarine = submarine.copy(position = submarine.position.plus(value),
-                                                                                depth = submarine.depth + submarine.aim.times(value))
-    }
-    class AimUp(val value: Int) : MoveCommand {
-        override fun execute(submarine: Submarine) : Submarine = submarine.copy(aim = submarine.aim.minus(value))
-    }
-    class AimDown(val value: Int) : MoveCommand {
-        override fun execute(submarine: Submarine) : Submarine = submarine.copy(aim = submarine.aim.plus(value))
-    }
+    internal fun moveHorizontal(value: Int): Submarine = copy(position = position.plus(value))
+    internal fun moveVertical(value: Int): Submarine = copy(depth = depth + aim.times(value))
+    internal fun aimUp(value: Int): Submarine = copy(aim = aim.minus(value))
+    internal fun aimDown(value: Int): Submarine = copy(aim = aim.plus(value))
 
     fun solution(): Int = position * depth
 }
 
+interface MoveCommand{
+    fun execute(submarine: Submarine) : Submarine
+}
+class MoveForward(val value: Int) : MoveCommand {
+    override fun execute(submarine: Submarine) : Submarine =
+            submarine.moveHorizontal(value)
+                     .moveVertical(value)
+}
+class AimUp(val value: Int) : MoveCommand {
+    override fun execute(submarine: Submarine) : Submarine =
+            submarine.aimUp(value)
+}
+class AimDown(val value: Int) : MoveCommand {
+    override fun execute(submarine: Submarine) : Submarine =
+            submarine.aimDown(value)
+}
+
+fun String.asCommand(): MoveCommand {
+    val (command,value) = this.trim().split(" ")
+            .windowed(2)
+            .map {(comm,value) -> comm to value.toInt()}
+            .first()
+    when (command) {
+        "forward" -> return MoveForward(value)
+        "up" -> return AimUp(value)
+        "down" -> return AimDown(value)
+    }
+    throw IllegalMoveException()
+}
 
 class IllegalMoveException : Throwable()
 
