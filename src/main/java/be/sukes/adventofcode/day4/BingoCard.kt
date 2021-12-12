fc5d70bb6a9142ce352eb1322234e72a502ea55b
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-12 18:43:08 +0100

    Day 4: mark number + isBingo for rows

--- src/main/java/be/sukes/adventofcode/day4/BingoCard.kt
+++ src/main/java/be/sukes/adventofcode/day4/BingoCard.kt
@@ -4,7 +4,15 @@ class BingoCard(cardString: List<String>) {
 
     val rows : List<CardRow>  = toCardRowList(cardString)
 
-    data class CardNumber(val value: Int, val marked: Boolean = false)
+    fun draw(inputNumber: String): BingoCard {
+        this.rows.forEach { row ->
+            row.numbers.forEach {number ->
+                if (number.value == inputNumber.toInt())
+                    number.mark()
+            }
+        }
+        return this
+    }
 
     private fun toCardRowList(cardString: List<String>): List<CardRow> {
         return cardString
@@ -12,11 +20,19 @@ class BingoCard(cardString: List<String>) {
                                             .filter { it.isNotBlank() })}
     }
 
+    fun isBingo(): Boolean =
+        rows.map { it.allMarked() }.filter { true }.first()
+
+    fun drawList(drawList: List<String>): BingoCard {
+        drawList.forEach { this.draw(it) }
+        return this
+    }
+
 
     class CardRow(rowStrings: List<String> ) {
-        val numbers : List<CardNumber>  = toCardNumberList(rowStrings)
+        val numbers = toCardNumberList(rowStrings)
 
-        private fun toCardNumberList(rowString : List<String>): List<CardNumber> =
+        private fun toCardNumberList(rowString : List<String>) =
                 rowString.map { CardNumber(it.toInt()) }
 
         override fun equals(other: Any?): Boolean {
@@ -34,25 +50,16 @@ class BingoCard(cardString: List<String>) {
             return numbers.hashCode()
         }
 
+        fun allMarked() =
+                numbers.filter { it.marked }.size == numbers.size
 
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as BingoCard
-
-        if (rows != other.rows) return false
-
-        return true
+    data class CardNumber(val value: Int, var marked: Boolean = false) {
+        fun mark() {
+            marked = true
+        }
     }
-
-    override fun hashCode(): Int {
-        return rows.hashCode()
-    }
-
-
 }
 
 
--- src/test/java/be/sukes/adventofcode/day4/Day4BingoTest.kt
+++ src/test/java/be/sukes/adventofcode/day4/Day4BingoTest.kt
@@ -1,5 +1,6 @@
 package be.sukes.adventofcode.day4
 
+import be.sukes.adventofcode.day4.BingoCard.CardNumber
 import be.sukes.adventofcode.day4.BingoCard.CardRow
 import be.sukes.adventofcode.import.FileReader
 import org.assertj.core.api.Assertions.assertThat
@@ -11,7 +12,7 @@ class Day4BingoTest {
     fun `create BingoCards - matching rows for first card`() {
         val cardsString = FileReader().readLines("/day4/testCards.txt")
 
-        val actual = parseCardsString(cardsString)
+        val actual = parseCards(cardsString)
 
         assertThat(actual).hasSize(3)
         assertThat(actual[0].rows).contains(CardRow(listOf("22","13","17","11","0")),
@@ -23,7 +24,35 @@ class Day4BingoTest {
 
     }
 
-    private fun parseCardsString(cardsString: List<String>): List<BingoCard> =
+    @Test
+    fun `draw 8 - 8 is marked, 23 is not`() {
+        val cardsString = FileReader().readLines("/day4/testCards.txt")
+
+        val actual = parseCards(cardsString)[0].draw("8")
+
+        assertThat(actual.rows[1].numbers).contains(CardNumber(8,true),
+                                                    CardNumber(23,false))
+    }
+
+    @Test
+    fun `draw 8 - check if Bingo - no bingo`() {
+        val cardsString = FileReader().readLines("/day4/testCards.txt")
+
+        val actual = parseCards(cardsString)[0].draw("8")
+
+        assertThat(actual.isBingo()).isFalse
+    }
+
+    @Test
+    fun `draw "22","13","17","11","0" - check if Bingo - bingo`() {
+        val cardsString = FileReader().readLines("/day4/testCards.txt")
+
+        val actual = parseCards(cardsString)[0].drawList(listOf("22","13","17","11","0"))
+
+        assertThat(actual.isBingo()).isTrue
+    }
+
+    private fun parseCards(cardsString: List<String>): List<BingoCard> =
             cardsString.filter { it.isNotBlank() }
                     .windowed(5,5)
                     .map { BingoCard(it) }
