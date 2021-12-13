f733aeb26a790f96ee554a946f10be73e628c00a
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-13 22:44:31 +0100

    Day 5: addition of valid, directional GridSpots on addingVentLine (dirty)

--- src/main/java/be/sukes/adventofcode/day5/Day5Vents.kt
+++ src/main/java/be/sukes/adventofcode/day5/Day5Vents.kt
@@ -1,17 +1,83 @@
 package be.sukes.adventofcode.day5
 
+class VentGrid{
+    var grid : MutableList<GridSpot> = mutableListOf()
+
+    fun traceLine(lineString: String)  {
+        addVentLine(lineString.toVentLines().first())
+    }
+
+    data class GridSpot(val coordinate: Coordinate, var numberOfVents : Int = 1 ) {
+        fun sameLocation(x: Int, y: Int) =
+                (coordinate.x == x) and (coordinate.y == y)
+
+        fun addVentLine() =
+            numberOfVents ++
+    }
+
+    /*0,2 -> 0,5*/
+    private fun addVentLine(ventLine: VentLine) {
+        if(ventLine is VerticalVentLine){
+            for(i in ventLine.start.y .. ventLine.end.y){
+                addVerticalVentLine(ventLine, i)
+            }
+        }else if(ventLine is HorizontalVentLine){
+            for(i in ventLine.start.x .. ventLine.end.x){
+                addHorizontalVentLine(ventLine, i)
+            }
+        }
+    }
+
+    private fun addVerticalVentLine(ventLine: VentLine, i: Int) {
+        val spotAtLocation = grid.filter { spot -> spot.sameLocation(ventLine.start.x, i) }
+        if (spotAtLocation.isEmpty()) {
+            grid.add(GridSpot(Coordinate(ventLine.start.x, i)))
+        } else {
+            spotAtLocation.first().addVentLine()
+        }
+    }
+
+    private fun addHorizontalVentLine(ventLine: VentLine, i: Int) {
+        val spotAtLocation = grid.filter { spot -> spot.sameLocation(i,ventLine.start.y) }
+        if (spotAtLocation.isEmpty()) {
+            grid.add(GridSpot(Coordinate(i,ventLine.start.y)))
+        } else {
+            spotAtLocation.first().addVentLine()
+        }
+    }
+
+
+}
+
 data class Coordinate(val x : Int, val y : Int)
-data class VentLine(val start: Coordinate, val end: Coordinate)
+open class VentLine(open val start: Coordinate, open val end: Coordinate)
+data class HorizontalVentLine(override val start: Coordinate, override val end: Coordinate) : VentLine(start, end)
+data class VerticalVentLine(override val start: Coordinate, override val end: Coordinate) : VentLine(start, end)
 
 fun String.toVentLines(): List<VentLine> =
-        this.split(",", " -> ")
-                .asSequence()
-                .windowed(2,2)
-                .map { (x, y) -> Coordinate(x.toInt(), y.toInt()) }
+        toCoordinates()
                 .windowed(2)
-                .filter { (start,end) -> start.x == end.x || start.y == end.y }
-                .map { (start, end) -> VentLine(start , end) }
+                .filter { (start, end) ->
+                    start.x == end.x || start.y == end.y
+                }
+                .map { (start, end) ->
+                    if(start.x == end.x){
+                        VerticalVentLine(start , end)
+                    }else {
+                        HorizontalVentLine(start , end)
+                    }
+                }
                 .toList()
+
+private fun String.toCoordinates(): Sequence<Coordinate> {
+    return this.split(",", " -> ")
+            .asSequence()
+            .windowed(2, 2)
+            .map { (x, y) ->
+                Coordinate(x.toInt(), y.toInt())
+            }
+}
+
 
 
 
--- src/test/java/be/sukes/adventofcode/day5/Day5VentsTest.kt
+++ src/test/java/be/sukes/adventofcode/day5/Day5VentsTest.kt
@@ -1,16 +1,25 @@
 package be.sukes.adventofcode.day5
 
+import be.sukes.adventofcode.day5.VentGrid.GridSpot
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day5VentsTest {
 
     @Test
-    fun `0,9 to 5,9 - toVentLines - correct VentLine`() {
+    fun `0,9 to 5,9 - toVentLines - correct HorizontalVentLine`() {
         val actual = "0,9 -> 5,9".toVentLines()
 
         assertThat(actual).hasSize(1)
-        assertThat(actual[0]).isEqualTo(VentLine(Coordinate(0,9),Coordinate(5,9)))
+        assertThat(actual[0]).isEqualTo(HorizontalVentLine(Coordinate(0,9),Coordinate(5,9)))
+    }
+
+    @Test
+    fun `0,5 to 0,9 - toVentLines - correct VerticalVentLine`() {
+        val actual = "0,5 -> 0,9".toVentLines()
+
+        assertThat(actual).hasSize(1)
+        assertThat(actual[0]).isEqualTo(VerticalVentLine(Coordinate(0,5),Coordinate(0,9)))
     }
 
     @Test
@@ -19,4 +28,52 @@ class Day5VentsTest {
 
         assertThat(actual).hasSize(0)
     }
+
+    @Test
+    fun `create VentGrid - traceLine "0,2 to 0,5" - 4 gridSpots (vertical) added`() {
+        val ventGrid= VentGrid()
+        ventGrid.traceLine("0,2 -> 0,5")
+
+        assertThat(ventGrid.grid).hasSize(4)
+        assertThat(ventGrid.grid).contains(GridSpot(Coordinate(0, 2),1),
+                                            GridSpot(Coordinate(0, 3),1),
+                                            GridSpot(Coordinate(0, 4),1),
+                                            GridSpot(Coordinate(0, 5),1))
+    }
+
+    @Test
+    fun `create VentGrid - traceLine twice - 4 gridSpots (vertical) with numberOfVents = 2`() {
+        val ventGrid= VentGrid()
+        ventGrid.traceLine("0,2 -> 0,5")
+        ventGrid.traceLine("0,2 -> 0,5")
+
+        assertThat(ventGrid.grid).hasSize(4)
+        assertThat(ventGrid.grid).contains(GridSpot(Coordinate(0, 2),2),
+                                            GridSpot(Coordinate(0, 3),2),
+                                            GridSpot(Coordinate(0, 4),2),
+                                            GridSpot(Coordinate(0, 5),2))
+    }
+
+    @Test
+    fun `create VentGrid - traceLine "2,0 to 4,0" - 3 gridSpots (horizontal) added`() {
+        val ventGrid= VentGrid()
+        ventGrid.traceLine("2,0 -> 4,0")
+
+        assertThat(ventGrid.grid).hasSize(3)
+        assertThat(ventGrid.grid).contains(GridSpot(Coordinate(2, 0),1),
+                                            GridSpot(Coordinate(3, 0),1),
+                                            GridSpot(Coordinate(4, 0),1))
+    }
+
+//    @Test
+//    fun `create VentGrid - traceLine "0,5 to 0,2" - 4 gridSpots (vertical) added`() {
+//        val ventGrid= VentGrid()
+//        ventGrid.traceLine("0,5 -> 0,2")
+//
+//        assertThat(ventGrid.grid).hasSize(4)
+//        assertThat(ventGrid.grid).contains(GridSpot(Coordinate(0, 2),1),
+//                GridSpot(Coordinate(0, 3),1),
+//                GridSpot(Coordinate(0, 4),1),
+//                GridSpot(Coordinate(0, 5),1))
+//    }
 }
