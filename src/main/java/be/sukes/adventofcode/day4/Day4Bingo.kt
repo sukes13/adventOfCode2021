17888be783cf1a049d977af825d9cadf3b256f7b
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-13 18:40:22 +0100

    Day 4: refactoring for readability

--- src/main/java/be/sukes/adventofcode/day4/Day4Bingo.kt
+++ src/main/java/be/sukes/adventofcode/day4/Day4Bingo.kt
@@ -3,16 +3,23 @@ package be.sukes.adventofcode.day4
 class Day4Bingo{
 
     fun solution1(cardsString: List<String>,drawsString: String) : Int =
-        play(cardsString, drawsString).toSolution { this.keys.first()}
+            cardsString.play(drawsString).toSolution { this.keys.first()}
 
     fun solution2(cardsString: List<String>,drawsString: String) : Int =
-        play(cardsString, drawsString).toSolution { this.keys.last()}
+            cardsString.play(drawsString).toSolution { this.keys.last()}
 
     private fun MutableMap<Int, Pair<Int, Int>>.toSolution(preference: MutableMap<Int, Pair<Int, Int>>.() -> Int) =
          this[this.preference()]?.calculateSolution() ?: 0
 
-    private fun play(cardsString: List<String>, drawsString: String): MutableMap<Int, Pair<Int, Int>> {
-        val cards = cardsString.asBingoCards()
+    private fun parseDraws(drawsString: String): List<Int> =
+            drawsString.split(",")
+                       .map { it.toInt() }
+
+    private fun Pair<Int,Int>.calculateSolution(): Int =
+            this.first.times(this.second)
+
+    private fun List<String>.play(drawsString: String): MutableMap<Int, Pair<Int, Int>> {
+        val cards = this.asBingoCards()
         val draws = parseDraws(drawsString)
 
         val cardWinningDraw: MutableMap<Int,Pair<Int, Int>> = mutableMapOf()
@@ -23,16 +30,7 @@ class Day4Bingo{
         }
         return cardWinningDraw
     }
-
-    private fun parseDraws(drawsString: String): List<Int> =
-            drawsString.split(",")
-                       .map { it.toInt() }
-
-    private fun Pair<Int,Int>.calculateSolution(): Int =
-            this.first.times(this.second)
-
 }
-
 
 fun List<String>.asBingoCards(): List<BingoCard>  =
         this.filter { it.isNotBlank() }
