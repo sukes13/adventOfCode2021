ba869f35844158356ad2dc63a2512184f5018081
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-14 20:33:32 +0100

    Day 5: refactoring: reduced duplicate code with lambda

--- src/main/java/be/sukes/adventofcode/day5/VentGrid.kt
+++ src/main/java/be/sukes/adventofcode/day5/VentGrid.kt
@@ -36,47 +36,34 @@ data class Coordinate(val x : Int, val y : Int)
 
 abstract class VentLine(open val start: Coordinate, open val end: Coordinate) {
     abstract fun coordinates(): List<Coordinate>
+
+    protected fun coordinatesOfRange(range : IntProgression,directionPointer : (Int) -> Coordinate): MutableList<Coordinate> {
+        val coordinates : MutableList<Coordinate> = mutableListOf()
+        for(i in range){
+            coordinates.add(directionPointer(i))
+        }
+        return coordinates
+    }
 }
 data class HorizontalVentLine(override val start: Coordinate, override val end: Coordinate) : VentLine(start, end){
     override fun coordinates() : List<Coordinate> {
         val range = if (ascending(start,end)) start.x.rangeTo(end.x) else start.x.downTo(end.x)
-
-        val coordinates : MutableList<Coordinate> = mutableListOf()
-        for(i in range){
-            coordinates.add(Coordinate(i, start.y))
-        }
-        return coordinates
+        return coordinatesOfRange(range){ index: Int -> Coordinate(index,start.y)}
     }
 }
 data class VerticalVentLine(override val start: Coordinate, override val end: Coordinate) : VentLine(start, end){
     override fun coordinates() : List<Coordinate> {
         val range = if (ascending(start,end)) start.y.rangeTo(end.y) else start.y.downTo(end.y)
-
-        val coordinates : MutableList<Coordinate> = mutableListOf()
-        for(i in range){
-            coordinates.add(Coordinate(start.x,i))
-        }
-        return coordinates
+        return coordinatesOfRange(range){ index: Int -> Coordinate(start.x,index)}
     }
-
 }
+
+
+
+
 private fun ascending(start: Coordinate, end: Coordinate) = start.x + start.y < end.x + end.y
 
 fun String.toVentLine(): VentLine? =  toCoordinates().mapToVentLine()
-
-private fun Pair<Coordinate,Coordinate>.mapToVentLine(): VentLine? {
-        if (this.first.x == this.second.x) {
-            return VerticalVentLine(this.first, this.second)
-        } else if(this.first.y == this.second.y){
-            return HorizontalVentLine(this.first, this.second)
-        }
-        return null
-}
-
-private fun Pair<Coordinate,Coordinate>.filterOutInvalidLine(): Boolean =
-    this.first.x == this.second.x || this.first.y == this.second.y
-
-
 
 private fun String.toCoordinates(): Pair<Coordinate, Coordinate> {
     return this.split(",", " -> ")
@@ -86,4 +73,13 @@ private fun String.toCoordinates(): Pair<Coordinate, Coordinate> {
                 Coordinate(x.toInt(), y.toInt())
             }.zipWithNext()
             .first()
+}
+
+private fun Pair<Coordinate,Coordinate>.mapToVentLine(): VentLine? {
+    if (this.first.x == this.second.x) {
+        return VerticalVentLine(this.first, this.second)
+    } else if(this.first.y == this.second.y){
+        return HorizontalVentLine(this.first, this.second)
+    }
+    return null
 }
