3da3c132fa80f43769f540254698d6b06e9aef24
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-18 00:41:26 +0100

    Day 8: can detect all signal mappings - dirty

--- src/main/java/be/sukes/adventofcode/day8/Day8Displays.kt
+++ src/main/java/be/sukes/adventofcode/day8/Day8Displays.kt
@@ -1,16 +1,14 @@
 package be.sukes.adventofcode.day8
 
 fun List<String>.solutionOne() =
-        this.map { Display(it).detectEasyOutputDigits().size }
+        this.map { Display(it).outDigits.detectEasySignals().size }
                 .sum()
 
-class Display(entryString: String) {
-    private val entries = entryString.toEntries()
-    val inDigits = entries.first
-    val outDigits = entries.second
+fun List<String>.solutionTwo() =
+        0
 
-    fun detectEasyOutputDigits(): List<Signal> {
-        return outDigits.mapNotNull {
+fun List<String>.detectEasySignals(): List<Pair<Int,String>> =
+        this.mapNotNull {
             when (it.length) {
                 2 -> 1 to it
                 3 -> 7 to it
@@ -19,6 +17,30 @@ class Display(entryString: String) {
                 else -> null
             }
         }
+
+class Display(entryString: String) {
+    private val entries = entryString.toEntries()
+    val inDigits = entries.first
+    val outDigits = entries.second
+
+    fun detectSignals(): Map<Int,String> {
+        val solved = this.inDigits.detectEasySignals().toMap().toMutableMap()
+        var unsolved= this.inDigits.filterNot { solved.containsValue(it) }
+
+        solved[3] = unsolved.filter { it.length == 5 }.single { it.stripOfLetters(solved[1]!!).length  == 3 }
+        unsolved= unsolved.filterNot { it == solved[3] }
+
+        solved[5] = unsolved.filter { it.length == 5 }.single { it.stripOfLetters(solved[4]!!).length  == 2 }
+
+        solved[2] = unsolved.single { it != solved[5] && it.length == 5 }
+
+        solved[6] = unsolved.filter { it.length == 6 }.single { it.stripOfLetters(solved[1]!!).length  == 5 }
+        unsolved = unsolved.filterNot { it == solved[6] }
+
+        solved[9] = unsolved.filter { it.length == 6 }.single { it.stripOfLetters(solved[4]!!).length == 2 }
+        solved[0] = unsolved.single { it != solved[9] && it.length == 6 }
+
+        return solved
     }
 
     private fun String.toEntries() =
@@ -30,7 +52,25 @@ class Display(entryString: String) {
 
     private fun String.toStringList() =
             this.split(" ")
+                    .map { it }
+
+    private fun String.stripOfLetters(stripString : String) =
+            stripString.fold(this, {it , stripper ->
+                return@fold it.replace(stripper.toString(),"")
+            })
 
 }
 
-typealias Signal = Pair<Int,String>
+
+//
+//fun List<String>.detectEasySignalsd(): Map<Int,String> =
+//    this.toList().fold(mutableMapOf() , { signals, seg ->
+//        when (seg.length) {
+//            2 -> signals[1] = seg
+//            3 -> signals[7] = seg
+//            4 -> signals[4] = seg
+//            7 -> signals[8] = seg
+//            else -> null
+//        }
+//        return@fold signals
+//    })
--- src/test/java/be/sukes/adventofcode/day8/Day8DisplaysTest.kt
+++ src/test/java/be/sukes/adventofcode/day8/Day8DisplaysTest.kt
@@ -3,6 +3,7 @@ package be.sukes.adventofcode.day8
 import be.sukes.adventofcode.import.FileReader
 import org.assertj.core.api.SoftAssertions
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.entry
 import org.junit.jupiter.api.Test
 
 class Day8DisplaysTest {
@@ -19,24 +20,24 @@ class Day8DisplaysTest {
     }
 
     @Test
-    fun `test input first - detect digit = empty`() {
+    fun `test input first - detectEasySignals digit = empty`() {
         val display = Display("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
 
-        val actual = display.detectEasyOutputDigits()
+        val actual = display.outDigits.detectEasySignals()
 
         assertThat(actual).isEmpty()
     }
 
     @Test
-    fun `test input first - detect digit = 1`() {
+    fun `test input first - detectEasySignals all found`() {
         val display = Display("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe cgb gc")
 
-        val actual = display.detectEasyOutputDigits()
+        val actual = display.outDigits.detectEasySignals()
 
-        assertThat(actual).containsExactly(8 to "fdgacbe",
-                                            4 to "gcbe",
-                                            7 to "cgb",
-                                            1 to "gc")
+        assertThat(actual).containsExactly((8 to "fdgacbe"),
+                                    (4 to "gcbe"),
+                                    (7 to "cgb"),
+                                    (1 to "gc"))
     }
 
     @Test
@@ -56,5 +57,24 @@ class Day8DisplaysTest {
 
         assertThat(actual).isEqualTo(416)
     }
+
+    @Test
+    fun `test input first - detectSignals all found`() {
+        val display = Display("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+
+        val actual = display.detectSignals()
+
+        assertThat(actual).contains(entry(0 , "cagedb"),
+                                            entry(1 , "ab"),
+                                            entry(2 , "gcdfa"),
+                                            entry(3 , "fbcad"),
+                                            entry(4 , "eafb"),
+                                            entry(5 , "cdfbe"),
+                                            entry(6 , "cdfgeb"),
+                                            entry(7 , "dab"),
+                                            entry(8 , "acedgfb"),
+                                            entry(9 , "cefabd"))
+    }
+
 }
 
