2014c8ce1af043f2d755c00cee6cb14ca6e4edca
sukes13 <sir_jelle_wouters@hotmail.com>
2021-12-20 21:47:51 +0100

    Day 9: introduced recursive method + (following chain of) refactorings

--- src/main/java/be/sukes/adventofcode/day9/Day9HeightMap.kt
+++ src/main/java/be/sukes/adventofcode/day9/Day9HeightMap.kt
@@ -7,13 +7,11 @@ class HeightMap(input: List<String>) {
             lowPoints().map{it.height + 1}
                         .sum()
 
-    fun solutionTwo(): Int {
-        return lowPoints().asSequence().map { basinOf(it) }
-                                        .sortedByDescending { it.size }
-                                        .take(3)
-                                        .map { it.size }
-                                        .reduce(Int::times)
-    }
+    fun solutionTwo() =
+            lowPoints().asSequence().map { basinOf(it).size }
+                                    .sortedByDescending { it }
+                                    .take(3)
+                                    .reduce(Int::times)
 
     fun neighboursOf(location : Coordinate) =
             floor.filter { spot ->
@@ -31,48 +29,36 @@ class HeightMap(input: List<String>) {
                 }.size == neighbours.size
             }
 
-    fun basinOf(spot: Spot) : Set<Spot>  {
-        val basin : MutableSet<Spot> = mutableSetOf()
+    fun basinOf(spot: Spot) = fillBasin(mutableSetOf(spot))
 
-        var notNines = setOf(spot)
-        while (hasNewNotNineNeighbours(notNines, basin)){
-            basin.addToSpots(notNines)
-            notNines = notNines.map { newSpot ->
-                val newNotNines = neighboursNotNine(newSpot)
-
-                basin.addToSpots(newNotNines)
-                return@map newNotNines
-            }.flatten().toSet()
+    private tailrec fun fillBasin(basin : MutableSet<Spot>): MutableSet<Spot> {
+            val newSpots = basin.newBasinNeighbours()
+            return when {
+                        newSpots.isNotEmpty() -> {
+                            basin.addAll(newSpots)
+                            fillBasin(basin)
+                        }
+                        else  -> basin
+            }
         }
 
-        return basin
-    }
+    private fun Set<Spot>.newBasinNeighbours() =
+            this.flatMap { spot ->
+                        neighboursOf(spot.coordinate).filter { it.height != 9 }
+                    }
+                    .filterNot { it in this }
+                    .toSet()
 
     private fun List<String>.toFloor() =
             this.mapIndexed { y, line ->
-                       line.mapLine(y)
-                    }.flatMap { it.toSet() }.toSet()
+                line.mapLine(y)
+            }.flatMap { it.toSet() }.toSet()
 
     private fun String.mapLine(y: Int) =
             this.trim()
-                .mapIndexed { x, height ->
-                    Spot(Coordinate(x , y) , height.toString().toInt())
-                }
-
-    private fun hasNewNotNineNeighbours(notNines: Set<Spot>, basin: MutableSet<Spot>) =
-            notNines.flatMap { neighboursNotNine(it) }.filterNot { it in basin }.isNotEmpty()
-
-    private fun MutableSet<Spot>.addToSpots(notNine: Set<Spot>) {
-        println("Add to basin -> $notNine")
-        this.addAll(notNine)
-    }
-
-    private fun neighboursNotNine(spot: Spot): Set<Spot> {
-        return neighboursOf(spot.coordinate).filter { neighbour ->
-            neighbour.height != 9
-        }.toSet()
-    }
-
+                    .mapIndexed { x, height ->
+                        Spot(Coordinate(x , y) , height.toString().toInt())
+                    }
 }
 
 data class Coordinate(val x:Int,val y:Int)
